fb5c8627b3640e60ae5595dbf8023052a529fc62
Martin Komara <martin.komara@student.spseke.sk>
2025-06-03 20:33:03 +0200

    aaa

--- obj/Debug/net8.0-windows/WeatherApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WeatherApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d27eae1d418b920557fbdb63c85fb17132264cae")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
